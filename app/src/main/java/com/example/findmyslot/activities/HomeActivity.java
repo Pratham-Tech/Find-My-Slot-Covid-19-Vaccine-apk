74e89d151f8d0fa5d8d9841d14ea0a0140b6f263
Pratham Dhawan <pdhawan1975@gmail.com>
2021-06-01 19:00:01 +0530

    currently in development mode

---
+++ app/src/main/java/com/example/findmyslot/activities/HomeActivity.java
@@ -0,0 +1,119 @@
+package com.example.findmyslot.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.findmyslot.R;
+
+public class HomeActivity extends AppCompatActivity{
+
+    private EditText pincodeText;
+    private Spinner stateSpinner;
+    private Spinner districtSpinner;
+    private Button findSlots;
+    private RadioButton pincodeRadio;
+    private RadioButton districtRadio;
+    private Boolean ALL_SET = false;
+    private int radioId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Initialize();
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGrp);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId)
+            {
+                switch(checkedId)
+                {
+                    case R.id.radioButton1:
+
+                        stateSpinner.setVisibility(View.INVISIBLE);
+                        districtSpinner.setVisibility(View.INVISIBLE);
+                        pincodeText.setVisibility(View.VISIBLE);
+                        findSlots.setEnabled(true);
+                        radioId = checkedId;
+                        break;
+
+                    case R.id.radioButton2:
+                        pincodeText.setVisibility(View.INVISIBLE);
+                        stateSpinner.setVisibility(View.VISIBLE);
+                        districtSpinner.setVisibility(View.VISIBLE);
+                        findSlots.setEnabled(true);
+                        radioId = checkedId;
+                        break;
+                }
+            }
+        });
+
+        findSlots.setOnClickListener(view ->{
+
+            validate(radioId);
+
+            if(ALL_SET)
+                    startActivity(new Intent(HomeActivity.this,ResultActivity.class));
+        });
+    }
+
+    private void Initialize(){
+
+        pincodeRadio = findViewById(R.id.radioButton1);
+        districtRadio = findViewById(R.id.radioButton2);
+        pincodeText = findViewById(R.id.pincodeInput);
+        stateSpinner = findViewById(R.id.listState);
+        districtSpinner = findViewById(R.id.listDistrict);
+        findSlots = findViewById(R.id.findSlots);
+
+        pincodeText.setVisibility(View.INVISIBLE);
+        stateSpinner.setVisibility(View.INVISIBLE);
+        districtSpinner.setVisibility(View.INVISIBLE);
+        findSlots.setEnabled(false);
+
+        }
+
+    private void validate(int checkedId){
+
+        if(checkedId == R.id.radioButton1){
+
+            if (TextUtils.isEmpty(pincodeText.getText().toString()) || pincodeText.getText().length() < 6) {
+
+                Toast.makeText(getApplicationContext(),"Please enter any valid pincode",Toast.LENGTH_SHORT).show();
+                ALL_SET = false;
+            }
+            else
+                ALL_SET = true;
+        }
+        else{
+
+            if (stateSpinner.getSelectedItem()== null || districtSpinner.getSelectedItem()== null) {
+
+                Toast.makeText(getApplicationContext(),"Please select your district",Toast.LENGTH_SHORT).show();
+                ALL_SET = false;
+            }
+            else if(stateSpinner.getSelectedItem().toString().equals("Select State") || districtSpinner.getSelectedItem().toString().equals("Select District")){
+
+                Toast.makeText(getApplicationContext(),"Please select your district",Toast.LENGTH_SHORT).show();
+                ALL_SET = false;
+            }
+            else
+                ALL_SET = true;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/findmyslot/activities/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.findmyslot.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.findmyslot.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+             public void run(){
+
+                 Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+
+                 startActivity(intent);
+                 finish();
+             }
+             },2000);
+    }
+}
